09868ae9a5a8d31fd5797a915a07e303bd2cdf7d
seanw2020 <seanw2020@users.noreply.github.com>
2021-06-26 20:30:21 -0500

    prepare for calling this as a module

--- istio/main.tf
+++ istio/main.tf
@@ -47,7 +47,6 @@ data "aws_eks_cluster_auth" "cluster" {
   name = local.cluster_name
 }
 
-#
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
--- istio/variables.tf
+++ istio/variables.tf
@@ -9,3 +9,9 @@ variable "remote_state_bucket" {
 variable "remote_state_key" {
   default = "eks/terraform.tfstate"
 }
+
+variable "role_arn" {
+  description = "assume this role when provisioning."
+  # something like this:
+  # role_arn = "arn:aws:iam::${get_aws_account_id()}:role/terragrunt"
+}
