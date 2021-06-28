5d4f07523472174423a418345af911caf9fe917b
seanw2020 <seanw2020@users.noreply.github.com>
2021-06-27 21:50:02 -0500

    add kubectl provider

--- istio/main.tf
+++ istio/main.tf
@@ -7,6 +7,11 @@ terraform {
     aws        = ">= 3.22.0"
     kubernetes = "~> 1.11"
     helm       = ">= 2.2.0"
+
+    kubectl = {
+      source = "gavinbunney/kubectl"
+      version = ">= 1.11.2"
+    }
   }
 }
 
@@ -61,27 +66,21 @@ resource "helm_release" "istio-operator" {
   namespace  = "istio-system"
   create_namespace = true
 
-  values = [
-    file("${path.module}/configure.yaml")
-  ]
+  # values = [
+  #   file("${path.module}/configure.yaml")
+  # ]
 }
 
-# resource "helm_release" "istiod" {
-#   name       = "istiod"
-#   chart      = "release/manifests/charts/istio-control/istio-discovery"
-#   namespace  = "istio-system"
-#   create_namespace = true
-# }
+provider "kubectl" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args = ["eks", "get-token", "--cluster-name", var.cluster_name]
+    command = "aws"
+  }
+}
 
-# resource "helm_release" "istio-ingress" {
-#   name       = "istio-ingress"
-#   chart      = "release/manifests/charts/gateways/istio-ingress"
-#   namespace  = "istio-system"
-#   create_namespace = true
-# }
-
-# resource "helm_release" "istio-egress" {
-#   name       = "istio-egress"
-#   chart      = "release/manifests/charts/gateways/istio-egress"
-#   create_namespace = true
-# }
+resource "kubectl_manifest" "operator" {
+    yaml_body = file("${path.module}/configure.yaml")
+}
